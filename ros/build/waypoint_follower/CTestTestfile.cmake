80d25f37db8bf65deea2c3703e0519d736778361
hayoung kim <altairyoung@gmail.com>
2017-12-28 14:38:00 +0900

    remove devel and build directory

--- ros/build/waypoint_follower/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hayoung/carnd/capstone/ros/src/waypoint_follower
-# Build directory: /home/hayoung/carnd/capstone/ros/build/waypoint_follower
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros/build/waypoint_updater/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hayoung/carnd/capstone/ros/src/waypoint_updater
-# Build directory: /home/hayoung/carnd/capstone/ros/build/waypoint_updater
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
