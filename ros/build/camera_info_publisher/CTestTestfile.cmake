12140c64f50aec139e498018cdd9b858f96edfad
Kyushik <kyushikmin@gmail.com>
2017-12-28 16:51:05 +0900

    delete devel and build

--- ros/build/camera_info_publisher/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyushik/Desktop/udacity-carnd-capstone/ros/src/camera_info_publisher
-# Build directory: /home/kyushik/Desktop/udacity-carnd-capstone/ros/build/camera_info_publisher
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros/build/waypoint_loader/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyushik/Desktop/udacity-carnd-capstone/ros/src/waypoint_loader
-# Build directory: /home/kyushik/Desktop/udacity-carnd-capstone/ros/build/waypoint_loader
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
